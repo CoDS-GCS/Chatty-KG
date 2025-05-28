86e0113ac22ca707fc7afc6892b8bbb662a3e8f6
Reham-Osama <reham_osama93@yahoo.com>
2025-05-28 18:55:46 -0400

    Added Independent functions for each agent

---
+++ src/multi-agent/modules/State.py
@@ -0,0 +1,207 @@
+from langchain.schema.output_parser import StrOutputParser
+from langchain_openai import ChatOpenAI
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage
+from pydantic import BaseModel, Field
+from typing import List
+
+from chattykg.chattykg import knowledge_graph_to_uri
+from chattykg.json_logger import JsonLogger
+from chattykg.sparql_end_points.EndPoint import EndPoint
+from chattykg.sparql_end_points.XML_EndPoint import XML_EndPoint
+
+
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    """In memory implementation of chat message history."""
+
+    messages: List[BaseMessage] = Field(default_factory=list)
+
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        """Add a list of messages to the store"""
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+
+class State:
+    def __init__(self, knowledge_graph, n_limit_VQuery, n_max_Vs, n_limit_EQuery, n_max_Es, n_max_answers, filtration_enabled):
+        self.string_output_parser = StrOutputParser()
+        self.chatbot_llm = ChatOpenAI(model="gpt-4o", temperature=0.5)
+        self.store = {}
+
+        self.json_logger = JsonLogger()
+        # (Question Object not the question text)
+        self.question = None
+        self.target_variable = None
+        self.knowledge_graph = knowledge_graph
+        self.sparql_end_point = self.set_up_sparql_endpoint(knowledge_graph)
+        self.n_max_Vs = n_max_Vs
+        self.n_max_Es = n_max_Es
+        self.n_limit_VQuery = n_limit_VQuery
+        self.n_limit_EQuery = n_limit_EQuery
+        self.n_max_answers = n_max_answers
+        self.filteration_enabled = filtration_enabled
+
+
+    def set_up_sparql_endpoint(self, knowledge_graph):
+        if knowledge_graph in ["open_citations"]:
+            sparql_end_point = XML_EndPoint(
+                knowledge_graph, knowledge_graph_to_uri[knowledge_graph], self.filteration_enabled
+            )
+        else:
+            sparql_end_point = EndPoint(
+                knowledge_graph, knowledge_graph_to_uri[knowledge_graph], self.filteration_enabled
+            )
+        return sparql_end_point
+
+    def add_possible_answer(self, query, score, node_uris, relation_uris, triples):
+        self.question.add_possible_answer(
+            question=self.question.text, sparql=query, score=score, nodes=node_uris, edges=relation_uris,
+            triples=triples
+        )
+
+    def set_question(self, question):
+        self.question = question
+
+
+    def set_target_variable(self, target_variable):
+        self.target_variable = target_variable
+
+    def get_chatbot_llm(self):
+        return self.chatbot_llm
+
+    def get_string_output_parser(self):
+        return self.string_output_parser
+
+    def get_by_session_id(self, session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self.store:
+            self.store[session_id] = InMemoryHistory()
+        return self.store[session_id]
+
+    def get_json_logger(self):
+        return self.json_logger
+
+    def get_query_graph(self):
+        return self.question.query_graph
+
+    def get_knowledge_graph(self):
+        return self.knowledge_graph
+
+    def get_n_limit_VQuery(self):
+        return self.n_limit_VQuery
+
+    def get_n_max_Vs(self):
+        return self.n_max_Vs
+
+    def get_n_limit_EQuery(self):
+        return self.n_limit_EQuery
+
+    def get_n_max_Es(self):
+        return self.n_max_Es
+
+    def get_sparql_end_point(self):
+        return self.sparql_end_point
+
+    def get_target_variable(self):
+        return self.target_variable
+
+    def get_question_text(self):
+        return self.question.text
+
+    def get_answers(self):
+        return self.question.possible_answers[: self.n_max_answers]
+
+    def get_answers_at_index(self, index):
+        return self.question.possible_answers[index]
+
+    def get_answer_datatype(self):
+        return self.question.answer_datatype
+
+    #  Adding this to not break the code, but it looks like that we only use the boolean datatype in our current version of the system.
+    def detect_question_and_answer_type(self):
+        if not self.question.answer_datatype:
+            self.question.answer_type = "string"
+            self.question.answer_datatype = "string"
+
+        if self.question.text.lower().startswith("who was"):
+            self.question.answer_type = "person"
+            self.question.answer_datatype = "resource"
+        elif self.question.text.lower().startswith("who is "):
+            self.question.answer_type = "person"
+            self.question.answer_datatype = "resource"
+        elif self.question.text.lower().startswith("are "):
+            self.question.answer_type = "boolean"
+            self.question.answer_datatype = "boolean"
+        elif self.question.text.lower().startswith("is "):
+            self.question.answer_type = "boolean"
+            self.question.answer_datatype = "boolean"
+        elif self.question.text.lower().startswith("did "):
+            self.question.answer_type = "boolean"
+            self.question.answer_datatype = "boolean"
+        elif self.question.text.lower().startswith("do "):
+            self.question.answer_type = "boolean"
+            self.question.answer_datatype = "boolean"
+        elif self.question.text.lower().startswith("does "):
+            self.question.answer_type = "boolean"
+            self.question.answer_datatype = "boolean"
+        elif self.question.text.lower().startswith("was "):
+            self.question.answer_type = "boolean"
+            self.question.answer_datatype = "boolean"
+        elif self.question.text.lower().startswith("were "):
+            self.question.answer_type = "boolean"
+            self.question.answer_datatype = "boolean"
+        elif self.question.text.lower().startswith("who are "):
+            self.question.answer_type = "person"
+            self.question.answer_datatype = "list"
+        elif self.question.text.lower().startswith("who "):  # Who [V]
+            self.question.answer_type = "person"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith("whom "):  # Who [V]
+            self.question.answer_type = "person"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith("how many "):
+            self.question.answer_type = "count"
+            self.question.answer_datatype = "number"
+        elif self.question.text.lower().startswith("how much "):
+            self.question.answer_type = "price"
+            self.question.answer_datatype = "number"
+        elif self.question.text.lower().startswith(
+            "when did "
+        ) or self.question.text.lower().startswith("when was "):
+            self.question.answer_type = "date"
+            self.question.answer_datatype = "date"
+        elif self.question.text.lower().startswith("when "):
+            self.question.answer_type = "date"
+            self.question.answer_datatype = "date"
+        elif self.question.text.lower().startswith("which airports "):  # where do
+            self.question.answer_type = "place"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith("which languages "):  # where do
+            self.question.answer_type = "language"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith("what languages "):  # where do
+            self.question.answer_type = "language"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith("which countries "):  # where do
+            self.question.answer_type = "place"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith(
+            "in which "
+        ):  # In which [NNS], In which city
+            self.question.answer_type = "place"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith(
+            "which "
+        ):  # which [NNS], which actors
+            self.question.answer_type = "other"
+            self.question.answer_datatype = "list"  # of list
+        elif self.question.text.lower().startswith("where "):  # where do
+            self.question.answer_type = "place"
+            self.question.answer_datatype = "resource"  # of list
+        elif self.question.text.lower().startswith("show "):  # Show ... all
+            self.question.answer_type = "other"
+            self.question.answer_datatype = "list"  # of list
+        else:
+            pass
+
---
+++ src/multi-agent/modules/utils.py
@@ -0,0 +1,205 @@
+from chattykg import sparqls
+import chattykg.embeddings_client as w2v
+import operator
+from chattykg.nlp.utils import remove_duplicates
+from itertools import product
+import networkx as nx
+from urllib.parse import urlparse
+import datetime
+
+import re
+import os
+
+
+def is_variable(label):
+    return label.startswith('?')
+
+def get_entity_query_for_kg(entity, knowledge_graph, n_limit_VQuery):
+    if knowledge_graph in ["microsoft_academic", "bgee"]:
+        entity_query = sparqls.make_Ms_academic_query(
+            entity, limit=n_limit_VQuery
+        )
+    elif knowledge_graph in ['yago']:
+        entity_query = sparqls.make_keyword_unordered_search_query_with_type_yago(
+            entity, limit=n_limit_VQuery)
+    else:
+        entity_query = sparqls.make_keyword_unordered_search_query_with_type(
+            entity, limit=n_limit_VQuery
+        )
+    return entity_query
+
+
+def __get_chosen_URIs_for_relation(relation: str, uris: list, names: list, n_max_Es: int) -> list:
+    if not uris:
+        return uris
+
+    scores = __compute_semantic_similarity_between_single_word_and_word_list(
+        relation, names
+    )
+    # (uri, vetrex, True) ===>  (uri, vertex, True, score)
+    l1, l2, l3 = list(zip(*uris))
+    URIs_with_scores = list(zip(l1, l2, l3, scores))
+    URIs_with_scores.sort(key=operator.itemgetter(3), reverse=True)
+    # print("Edges with scores")
+    return remove_duplicates(URIs_with_scores)[:n_max_Es]
+
+
+def __compute_semantic_similarity_between_single_word_and_word_list(
+    word, word_list
+):
+    scores = list()
+    score = 0.0
+    for w in word_list:
+        try:
+            score = w2v.n_similarity(word.lower().split(), w.lower().split())
+        except KeyError:
+            score = 0.0
+        finally:
+            scores.append(score)
+    else:
+        return scores
+
+
+def get_all_possible_triples_for_edge(edge, first_uris, second_uris):
+    possible_triples = list()
+    for predicate, vertex, orientation, score in edge:
+        if orientation:
+            # vertex is object
+            object_uris = [vertex]
+            if vertex in first_uris:
+                subject_uris = second_uris
+            elif vertex in second_uris:
+                subject_uris = first_uris
+        else:
+            # vertex is subject
+            subject_uris = [vertex]
+            if vertex in first_uris:
+                object_uris = second_uris
+            elif vertex in second_uris:
+                object_uris = first_uris
+        triples = list(product(subject_uris, [(predicate, score)], object_uris))
+        possible_triples.extend(triples)
+
+    # This means that we have a triple of this structure (var1 ?p var2)
+    if (
+        len(edge) == 0
+        and len(first_uris) > 0
+        and len(second_uris)
+        and is_variable(first_uris[0])
+        and is_variable(second_uris[0])
+    ):
+        possible_triples.append((first_uris[0], ("?p", 0), second_uris[0]))
+        possible_triples.append((second_uris[0], ("?p", 0), first_uris[0]))
+
+    return possible_triples
+
+def check_validity(triples):
+    if (
+        len(triples) == 1
+        and len(triples[0]) == 2
+        and is_variable(triples[0][0][0])
+        and is_variable(triples[0][0][2])
+        and is_variable(triples[0][1][0])
+        and is_variable(triples[0][1][2])
+    ):
+        print("In condition")
+        return False
+    return True
+
+
+def merge_tuples(product, tuple_list):
+    final_list = list()
+    for instance in product:
+        for item in tuple_list:
+            temp = instance
+            temp = temp + (item,)
+            final_list.append(temp)
+    return final_list
+
+def get_possible_combinations(query_graph):
+    edges = list(nx.dfs_edges(query_graph))
+    bgps = []
+    handled_edges = 0
+    for i in range(0, len(edges)):
+        current_triples = query_graph[edges[i][0]][edges[i][1]][0][
+            "possible_triples"
+        ]
+        if len(current_triples) == 0:
+            continue
+
+        if handled_edges == 0:
+            bgps = query_graph[edges[i][0]][edges[i][1]][0][
+                "possible_triples"
+            ]
+            handled_edges += 1
+            continue
+
+        connected_node = set(edges[i]).intersection(set(edges[i - 1]))
+        if len(connected_node) == 0:
+            continue
+        connected_node = next(iter(connected_node))
+        if is_variable(connected_node):
+            if len(bgps) == 0:
+                bgps = current_triples
+            elif handled_edges == 1:
+                bgps = product(bgps, current_triples)
+                bgps = list(bgps)
+            else:
+                bgps = merge_tuples(bgps, current_triples)
+
+        else:
+            connected_node_vertices = query_graph.nodes[
+                connected_node
+            ]["uris"]
+            updated_bgps = []
+            for triple in current_triples:
+                used_vertex_triple = (
+                    triple[0] if triple[0] in connected_node_vertices else triple[2]
+                )
+                for bgp in bgps:
+                    last_bgp_triple = bgp[len(bgp) - 1]
+                    used_bgp_vertex = (
+                        last_bgp_triple[0]
+                        if last_bgp_triple[0] in connected_node_vertices
+                        else last_bgp_triple[2]
+                    )
+                    if used_vertex_triple == used_bgp_vertex:
+                        temp = bgp.append(triple)
+                        updated_bgps.append(temp)
+            bgps = updated_bgps
+        handled_edges += 1
+
+    if handled_edges == 1:
+        bgps = list(product(bgps))
+    return bgps
+
+
+def calculate_score(star_query):
+    score = 0.0
+    score_count = 0
+    for q in star_query:
+        if len(q) == 2:
+            score += q[1][2]
+            score_count += 1
+        elif len(q) == 3:
+            score += q[1][1]
+            score_count += 1
+    return score / score_count if score_count > 0 else score
+
+
+def extract_resource_name_from_uri(uri: str):
+    resource_URI = uri
+    uri_path = urlparse(resource_URI).path
+    resource_name = os.path.basename(uri_path)
+    resource_name = re.sub(r"(:|_|\(|\))", " ", resource_name)
+    return resource_URI, resource_name
+
+def postprocess_answer_if_needed(v_result, target_variable):
+    target = target_variable[1: ] if target_variable.startswith('?') else target_variable
+    for binding in v_result["results"]["bindings"]:
+        if "datatype" in binding[target]:
+            if "gYear" in binding[target]["datatype"]:
+                if int(binding[target]["value"]) > 0:
+                    obj = datetime.datetime.strptime(binding[target]["value"], "%Y")
+                    binding[target]["value"] = str(obj.date())
+    return v_result
